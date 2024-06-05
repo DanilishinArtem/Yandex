d66029d9bd8c67c74893daafa28113090eaf6753
adanilishin <adanilishin@WIN-ARFHPLSLFR9>
2024-06-06 01:09:58 +0300

    ok

--- red/codeOptimization/test.cpp
+++ red/codeOptimization/test.cpp
@@ -4,11 +4,9 @@
 #include "lib.h"
 using namespace std;
 
+
+
 int main(){
-    ofstream out("output.txt");
-    for(size_t i = 0; i < 25; i++){
-        out << "London is the capital of Great Britain. "
-            << "I am travelling douwn the river"
-            << "\n";
-    }
+
+    return 0;
 }
